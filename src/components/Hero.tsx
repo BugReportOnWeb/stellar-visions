d099bd15ebba3c604eb97567c210b6f690495c83
Dev <flashytesting@gmail.com>
2023-10-08 02:18:40 +0530

    Add Hero component for Home page

---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+interface Props {
+    image: string
+    title: string
+    date: string
+}
+
+const Hero: React.FC<Props> = ({ image, title, date }) => {
+    return (
+        <div className='flex flex-col w-1/2 gap-7'>
+            <h1 className='text-[#C0C0C0] text-lg text-center'>Astronomy Picture of the Day</h1>
+
+            <Link to='/detail'>
+                <img
+                    className='shadow-xl h-[30rem] w-full rounded-lg cursor-pointer transition-transform duration-500 hover:scale-150'
+                    src={image}
+                />
+            </Link>
+
+            <div className='mx-3 text-[#B0B0B0] flex justify-between'>
+                <h1 className='text-sm'>{title}</h1>
+                <h1 className='text-sm'>{date}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
