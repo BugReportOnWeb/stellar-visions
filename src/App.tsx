c8b1ab75ead4dc022b32320d9e7b495624e6134a
Dev <flashytesting@gmail.com>
2023-10-18 21:55:34 +0530

    Provide root with api data context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+// Libraries/Modules
 import { Routes, Route } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
+// Context (Local Data)
+import { APIDataContexProvider } from "./context/APIDataContext";
+
+// Pages
 import Home from "./pages/Home";
 import Info from "./pages/Info";
-import { AnimatePresence } from "framer-motion";
 
 const App = () => {
     return (
-        <AnimatePresence>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/info' element={<Info />} />
-            </Routes>
-        </AnimatePresence>
+        <APIDataContexProvider>
+            <AnimatePresence>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/info' element={<Info />} />
+                </Routes>
+            </AnimatePresence>
+        </APIDataContexProvider>
     )
 }
 
