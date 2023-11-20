79f62fc74efe014ed92fe9ffbe79bdbf364c6f90
Dev <flashytesting@gmail.com>
2023-11-20 21:55:03 +0530

    Disable sourcemap view on production

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+
+    // TODO: Check later for possible bugs/errors
+    build: {
+        sourcemap: false
+    }
 })
